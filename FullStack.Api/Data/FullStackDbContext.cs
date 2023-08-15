e3ec55aa57c762a10798accec694a0415330f3a3
sujanpercoid <sujan.percoidit@gmail.com>
2023-08-15 17:33:32 +0545

    all curd backend

---
+++ FullStack.Api/Data/FullStackDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FullStack.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.Api.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee>Employees { get; set; }
+    }
+}
